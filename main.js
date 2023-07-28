9747c687eee16aa9a3d0f8a5eba0f77c9496f336
shauryadandwani <116278582+shauryadandwani@users.noreply.github.com>
2023-07-28 13:11:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Recognition = new SpeechRecognition();
+
+function start(){
+document.getElementById("textbox").innerHTML="";
+Recognition.start();
+}
+
+Recognition.onresult= function(event) {
+    
+    console.log(event)
+
+    var content = event.results[0][0].transcript;
+    console.log(content);
+
+    document.getElementById("textbox").innerHTML = content;
+    speak()
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data= document.getElementById("textbox").value;
+
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    Image_format:'png',
+    png_quality:90
+
+});
+camera=document.getElementById("camera")
